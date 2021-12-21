145faf5adfb29bd98b43f9d20500f6634974c31c
Claudio Nicolás Ale <ale.claudionicolas@gmail.com>
2021-12-21 15:43:57 +0000

    init.sh edited online with Bitbucket

--- init.sh
+++ init.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+function create_chain() {
+  NAME=$1
+  
+  iptables -N $NAME
+  iptables -F $NAME
+  iptables -X $NAME
+  iptables -I $NAME -j RETURN
+}
+
+function load_input_filters() {
+}
+
+apt update
+apt upgrade
+
+echo "please install knockd"
+
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+create_chain FORWARD_PORT_KNOCKING
+create_chain INPUT_FILTERS
+
+load_input_filters
+
+iptables -I FORWARD -j FORWARD_PORT_KNOCKING
+iptables -I INPUT -j INPUT_FILTERS
