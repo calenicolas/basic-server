ac0af8f66a90f44128b17e6f4a7727ffda5d2b18
claudio.ale <20777672+calenicolas@users.noreply.github.com>
2021-12-23 12:34:57 -0300

    init.sh edited online with Bitbucket

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ function load_input_filters() {
 
 apt update
 apt upgrade
-apt install knockd -y
+apt install iptables-persistent knockd -y
 
 echo "Ingrese la interfaz donde escucha actualmente el servicio de ssh"
 read SSH_INTERFACE
@@ -37,6 +37,8 @@ iptables -I INPUT -j INPUT_FILTERS
 SSH_CLIENT=$(echo $SSH_CLIENT | awk '{ print $1}')
 
 iptables -A INPUT -i $SSH_INTERFACE -p tcp --dport 22 -m state --state NEW,ESTABLISHED -s $SSH_CLIENT -j  ACCEPT
+
+iptables-save > /etc/iptables/rules.v4
 
 cp $SCRIPT_PATH/port-knocing/pk_accept_forward /usr/local/sbin
 cp $SCRIPT_PATH/port-knocing/pk_delete_forward /usr/local/sbin
